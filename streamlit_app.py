281bd960628f3311ea946e2f5b87835dd4e01117
nbgithub7 <55123997+nbgithub7@users.noreply.github.com>
2024-08-02 02:40:12 +0000

    yfinance

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,40 @@
+import requests
 import streamlit as st
+from datetime import datetime
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+#Header
+st.header("Trading Dashboard")
+
+#Accept the stock name and make an object for yfinance
+stockName = st.text_input("Enter Stock name",'')
+st.write("stock: " + stockName)
+
+stockName_yf = ''
+if stockName:
+    stockName_yf = yf.Ticker(str(stockName))
+    #stockName_yf.info
+
+#Download information, includes Adjusted Closed Price
+if stockName_yf:
+    #st.write("here")
+    stock_download = yf.download(str(stockName), period="max", interval="1mo")
+    #stock_download
+    #st.write("there")
+
+    stock_adjClose = stock_download[["Adj Close"]]
+    stock_adjClose.rename(columns={"Adj Close": "price_t"}, inplace=True)
+    
+    stock_adjClose["returns"] = stock_adjClose["price_t"].pct_change()
+    stock_adjClose["returns %"] = stock_adjClose["returns"] * 100
+
+    stock_adjClose
+
+
+
+    
+
+
